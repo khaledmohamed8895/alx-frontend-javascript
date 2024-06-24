683afc3f98998c0dfa7439a9a905ce3b23531feb
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-06-25 01:41:55 +0300

    finished task

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,10 @@
-export default function appendToEachArrayValue(array, appendString) {
-    const arr = [];
-    for (const idx of array) {
-      const value = idx;
-      arr.push(appendString + value);
-    }
-  
-    return arr;
+#!/usr/bin/node
+export default function appendToEachArrayValue (array, appendString) {
+  const arr = [];
+  for (const idx of array) {
+    const value = idx;
+    arr.push(appendString + value);
   }
+
+  return arr;
+}
