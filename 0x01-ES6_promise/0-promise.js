28e9c3219c4e381f90440e9d08106c8fe2f41947
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-06-26 00:54:06 +0300

    update

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,3 @@
-function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-      if (success) resolve({ status: 200, body: 'Success' });
-      reject(Error('The fake API is not working currently'));
-    });
-  }
-  
-  export default getFullResponseFromAPI;
+export default function getResponseFromAPI () {
+  return new Promise(() => {});
+}
